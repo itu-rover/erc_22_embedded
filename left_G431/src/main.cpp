a582e0ad148283d795b1655cf6452f49732279c4
Emir Danışman <76104355+EmirDns@users.noreply.github.com>
2023-02-25 12:42:07 +0300

    Update main.cpp

--- left_G431/src/main.cpp
+++ left_G431/src/main.cpp
@@ -5,10 +5,7 @@
 #include "std_msgs/Float64MultiArray.h"
 
 #define MOTOR_NUM 2
-#define CURRENT_COEFF 6
 #define RPM_COEFF 10000
-
-//RIGHT NOW IT'S ON DUTY CYCLE or CURRENT MODE
 
 //UART ports
 HardwareSerial LFserial(PC5, PC4);
@@ -19,15 +16,16 @@ VescUart LFmotor;
 VescUart LBmotor;
 
 //Standart variables
-float left_motors_command_float_array[MOTOR_NUM];
-float left_motors_current_command_float_array[MOTOR_NUM];
+float left_front_motor_speed;
+float left_back_motor_speed;
+unsigned long callback_start_time;
+//angel variable
+float angle;
 
 //ROS variables
 std_msgs::Float64MultiArray left_motors_feedback_multiarray;
-std_msgs::Float64MultiArray left_motors_command_multiarray;
 
 //Function declarations
-void mapCurrent(float *dest_command_array);
 void leftMotorsCallback(const std_msgs::Float64MultiArray &drive_system_command_multiarray);
 
 //ROS objects
@@ -35,7 +33,8 @@ ros::NodeHandle nh;
 ros::Subscriber <std_msgs::Float64MultiArray> leftMotorsSubscriber("drive_system/wheel_speed", &leftMotorsCallback);
 ros::Publisher leftMotorsPublisher("drive_system_left_motors_feedbacks", &left_motors_feedback_multiarray);
 
-void setup() {
+void setup() 
+{
   //ROS inits
   nh.initNode();
   nh.subscribe(leftMotorsSubscriber);
@@ -52,39 +51,50 @@ void setup() {
   //Memory allocation of the feedback multiarray
   left_motors_feedback_multiarray.data=(float *)malloc(sizeof(float)*MOTOR_NUM);  
   left_motors_feedback_multiarray.data_length=MOTOR_NUM;
-  
-  left_motors_command_multiarray.data=(float *)malloc(sizeof(float)*MOTOR_NUM);  
-  left_motors_command_multiarray.data_length=MOTOR_NUM;
+
+  callback_start_time=millis();
 }
 
-void loop() {
+void loop() 
+{
   nh.spinOnce();
   delay(1);
-  
-  LFmotor.setDuty(left_motors_command_float_array[0]);
-  LBmotor.setDuty(left_motors_command_float_array[1]);
-  
-  /*
-  LFmotor.setCurrent(left_motors_current_command_float_array[0]);
-  LBmotor.setCurrent(left_motors_current_command_float_array[1]);
-  */
-  /*
-  LFmotor.setRPM(left_motors_command_float_array[0]);
-  LBmotor.setRPM(left_motors_command_float_array[1]);
-  */
-  }
 
-void leftMotorsCallback(const std_msgs::Float64MultiArray &drive_system_command_multiarray){
-  for(int motor_counter=0;motor_counter<MOTOR_NUM;motor_counter++){
-    left_motors_command_float_array[motor_counter]=drive_system_command_multiarray.data[0];
-    left_motors_current_command_float_array[motor_counter]=drive_system_command_multiarray.data[0]*CURRENT_COEFF;
+  unsigned long loop_start_time=millis();
+  
+  if(!nh.connected() || (loop_start_time-callback_start_time)>500)
+  {
+    LFmotor.setRPM(0);
+    LBmotor.setRPM(0);
   }
+  
+  
+  //Brake according to the ramp angle
+  else if((nh.connected()) && fabs(left_front_motor_speed) <= 2 && fabs(left_back_motor_speed) <= 2 && angle>5)
+  {
+    LFmotor.setHandBrakeCurrent(6); // give 3 amps for braking for 45 degree
+    LBmotor.setHandBrakeCurrent(6);
+  }
+  
 
-  if(LFmotor.getVescValues() && LBmotor.getVescValues()){
+  else{
+    LFmotor.setRPM(left_front_motor_speed);
+    LBmotor.setRPM(left_back_motor_speed);
+  }
+}
+
+void leftMotorsCallback(const std_msgs::Float64MultiArray &drive_system_command_multiarray)
+{
+  callback_start_time=millis();
+  angle = drive_system_command_multiarray.data[4];
+  left_front_motor_speed = drive_system_command_multiarray.data[0]*RPM_COEFF;
+  left_back_motor_speed = drive_system_command_multiarray.data[1]*RPM_COEFF;
+
+  if(LFmotor.getVescValues() && LBmotor.getVescValues())
+  {
     left_motors_feedback_multiarray.data[0]=float(LFmotor.data.rpm/70);
     left_motors_feedback_multiarray.data[1]=float(LBmotor.data.rpm/70);
   }
 
   leftMotorsPublisher.publish(&left_motors_feedback_multiarray);
 }
-
